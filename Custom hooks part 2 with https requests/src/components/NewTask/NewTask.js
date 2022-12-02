4860c39243b9c99b6eb13dfc63fd6481f09fc27b
Gude Saikishore (MINDTREE LIMITED) <v-gudes@microsoft.com>
2022-12-02 08:53:14 +0530

    custom hooks

---
+++ Custom hooks part 2 with https requests/src/components/NewTask/NewTask.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import useHttp from "../../hooks/use-https";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendPostRequest } = useHttp();
+  const createTask = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendPostRequest(
+      {
+        url: "https://react-https-task-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
